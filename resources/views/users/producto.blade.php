a04d24c9bd5774f4dc9fa72eacd0405322a22d1b
LopzJavierEnriqz <javilop.lc@gmail.com>
2023-08-31 14:52:15 -0600

    vista producto usuario

--- resources/views/users/producto.blade.php
+++ resources/views/users/producto.blade.php
@@ -1,6 +1,56 @@
 @extends('layouts.app')
 @extends('layouts.components.navbar')
 @section('content')
+<div>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Vive una Nueva Experiencia</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Link
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Link</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<br>
+<h2>Nombre de la cabaña</h2>
+</div>
+<div class="float-right">
+<div class="container">
+<button type="button" class="btn btn-primary ml-auto" class="btn btn-outline-success" class="btn btn-primary move-left"><i class="fas fa-share-from-square"></i> Compartir</button>
+</div>
+</div>
+
+
+<button type="button" class="btn btn-outline-success"><i class="fas fa-heart"></i> Guardar</button>
+
+
 
 
 
