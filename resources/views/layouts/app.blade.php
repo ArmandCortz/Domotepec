32df096792f595b02847822a4564840b627e61d1
ArmandCortz <armand1515.lc@gmail.com>
2023-09-05 15:17:09 -0600

    version 1.1.1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,34 +2,10 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title> {{ env('APP_NAME') }} </title>
-    <!-- Scripts -->
-    <!-- CSS-->
-    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
-    {{-- <link href="{{ mix('/css/app.css') }}" rel="stylesheet"> --}} 
-    {{-- <link href="{{ asset('css/adminlte.min.css') }}" rel="stylesheet"> --}}
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
-    {{-- <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css"> --}}
-    {{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> --}}
-    {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> --}}
-    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script> --}}
-    {{-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> --}}
-
-
-    <style>
-        body {
-            font-family: 'Nunito', sans-serif;
-        }
-    </style>
+    
+    
+    @include('layouts.components.head')
+    
 </head>
 
 <body class="antialiased">
@@ -41,11 +17,10 @@
             @yield('content')
         </main>
     </div>
-    @yield('footer')
-    @push('scripts')
-        <script src="/js/app.js"></script>
 
-        <script src="{{ asset('js/diseño.js') }}"></script>
+
+    @push('scripts')
+        @include('layouts.components.scripts')
 
     </body>
 
